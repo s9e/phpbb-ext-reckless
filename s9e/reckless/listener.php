00bb2f9c7744355ab262d2db95cad5bfa09fd6f8
JoshyPHP <s9e.dev@gmail.com>
2019-09-26 16:34:19 +0200

    WiP

--- s9e/reckless/listener.php
+++ s9e/reckless/listener.php
@@ -45,7 +45,7 @@ class listener implements EventSubscriberInterface
 	*/
 	public function onViewforumCutoffQuery($event)
 	{
-		$regexp = '(\\s*OR t.topic_type = 2\\s*OR t.topic_type = 3)';
+		$regexp = '(\\s*OR t.topic_type = ' . POST_ANNOUNCE . '\\s*OR t.topic_type = ' . POST_GLOBAL . ')';
 
 		$sql = $event['sql_array'];
 		if ($sql['SELECT'] !== 'COUNT(t.topic_id) AS num_topics' || !preg_match($regexp, $sql['WHERE']))
